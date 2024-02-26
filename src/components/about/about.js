063afd96dc764a4b1e3feb3b8d43579760b8e2e4
b-lapointe <brittany.lapointe60@gmail.com>
2024-02-26 18:24:32 -0400

    Add new skill

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,16 +3,7 @@ import scrollTo from 'gatsby-plugin-smoothscroll'
 
 import './about.scss'
 import profile from '../../assets/profile.jpg'
-import html from '../../assets/html.png'
-import css from '../../assets/css.png'
-import javascript2 from '../../assets/javascript2.png'
-import angular from '../../assets/angular.png'
-import react from '../../assets/react.png'
-import wordpress from '../../assets/wordpress.png'
-import gatsby from '../../assets/gatsby.png'
-import adobeXD from '../../assets/adobeXD.png'
-import adobeIll from '../../assets/adobeIllustrator.png'    
-import sass from '../../assets/sass.png'    
+import skillData from './skillData';
 
 const aboutContent = require('./aboutContent.txt')
 
@@ -32,16 +23,17 @@ const About = () => {
                 </div>
             </div>
             <div className={'about__skills'}>
-                    <img className={'about__skills-icon'} src={html} alt={"HTML icon"} title="HTML" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={css} alt={"CSS icon"} title="CSS" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={javascript2} alt={"JavaScript icon"} title="JavaScript" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={react} alt={"React icon"} title="React" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={angular} alt={"Angular icon"} title="Angular" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={sass} alt={"SASS icon"} title="SASS" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={wordpress} alt={"Wordpress icon"} title="Wordpress" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={gatsby} alt={"Gatsby icon"} title="Gatsby" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={adobeIll} alt={"Adobe Illustrator icon"} title="Adobe Illustrator" height={'200px'} width={'200px'}/>
-                    <img className={'about__skills-icon'} src={adobeXD} alt={"Adobe XD icon"} title="Adobe XD" height={'200px'} width={'200px'}/>
+                {skillData.map((skill, index) => (
+                    <img
+                        key={index}
+                        className={'about__skills-icon'}
+                        src={skill.imgSrc}
+                        alt={skill.altText}
+                        title={skill.title}
+                        height={skill.height}
+                        width={skill.width}
+                    />
+                ))}
             </div>
         </div>
     )
---
+++ src/components/about/skillData.js
@@ -0,0 +1,93 @@
+import html from '../../assets/html.png'
+import css from '../../assets/css.png'
+import javascript2 from '../../assets/javascript2.png'
+import angular from '../../assets/angular.png'
+import react from '../../assets/react.png'
+import wordpress from '../../assets/wordpress.png'
+import gatsby from '../../assets/gatsby.png'
+import adobeXD from '../../assets/adobeXD.png'
+import adobeIll from '../../assets/adobeIllustrator.png'    
+import sass from '../../assets/sass.png'
+import php from '../../assets/php.png'
+
+const skillData = [
+    {
+        imgSrc: html,
+        altText: 'HTML logo',
+        title: 'HTML',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: css,
+        altText: 'CSS logo',
+        title: 'CSS',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: sass,
+        altText: 'Sass logo',
+        title: 'Sass',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: javascript2,
+        altText: 'JavaScript logo',
+        title: 'JavaScript',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: php,
+        altText: 'PHP logo',
+        title: 'PHP',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: react,
+        altText: 'React logo',
+        title: 'React',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: angular,
+        altText: 'Angular logo',
+        title: 'Angular',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: wordpress,
+        altText: 'WordPress logo',
+        title: 'WordPress',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: gatsby,
+        altText: 'Gatsby logo',
+        title: 'Gatsby',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: adobeXD,
+        altText: 'Adobe XD logo',
+        title: 'Adobe XD',
+        height: '200px',
+        width: '200px',
+    },
+    {
+        imgSrc: adobeIll,
+        altText: 'Adobe Illustrator logo',
+        title: 'Adobe Illustrator',
+        height: '200px',
+        width: '200px',
+    },
+]
+
+export default skillData
